1d89e0a40b181e2d00f4ba5ad3bfbc5d115a5621
Cláudio Gomes <cla.gomess@gmail.com>
2019-04-29 21:08:29 -0300

    fixes

--- src/main/java/br/jmonstro/bean/postman/collection/request/Param.java
+++ src/main/java/br/jmonstro/bean/postman/collection/request/Param.java
@@ -1,8 +1,10 @@
 package br.jmonstro.bean.postman.collection.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties({"name"})
 public class Param {
     private String key;
     private String value;
